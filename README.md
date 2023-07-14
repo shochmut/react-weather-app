d37b944ea92729977ed1e70cd1d48d5587923bfe
shochmut <50213094+shochmut@users.noreply.github.com>
2023-07-14 16:15:24 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-weather-app
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-f65dhv)
---
+++ src/CityInput.jsx
@@ -0,0 +1,17 @@
+import TextField from '@mui/material/TextField';
+import { useState } from 'react'
+
+
+function CityInput() {
+  const [city, setCity] = useState()
+
+
+  return(
+    <>
+      <TextField>
+      </TextField>
+    </>
+
+  )
+
+}
