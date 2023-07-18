3a439bf3a86efae2d18997048ce8cf1913a0a955
shochmut <50213094+shochmut@users.noreply.github.com>
2023-07-18 08:30:48 -0600

    Fixed up CityInput Component

--- src/CityInput.jsx
+++ src/CityInput.jsx
@@ -1,18 +1,18 @@
 import TextField from '@mui/material/TextField';
-import React from 'react';
+import { useState, useEffect } from 'react';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import GEODB_OPTIONS from './geodbAPI.jsx';
-import GEODB_URL from './geodbAPI.jsx';
+import { GEODB_OPTIONS } from './geodbAPI.jsx';
+import { GEODB_URL } from './geodbAPI.jsx';
 
 function CityInput() {
   const [cities, setCities] = useState([]);
 
   useEffect(() => {
-      fetch(GEODB_URL, GEODB_OPTIONS);
-      .then((response) => response.json());
+    fetch(GEODB_URL, GEODB_OPTIONS)
+      .then((response) => response.json())
       .then((json) => setCities(json.data));
   }, []);
   console.log(cities);
--- src/geodbAPI.jsx
+++ src/geodbAPI.jsx
@@ -1,11 +1,9 @@
-const GEODB_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions';
-const GEODB_OPTIONS = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '15e0453b2dmsh19dc8449e94fae8p13e049jsn03c45f5ce9b9',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
+export const GEODB_URL =
+  'https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions';
+export const GEODB_OPTIONS = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '15e0453b2dmsh19dc8449e94fae8p13e049jsn03c45f5ce9b9',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  },
 };
-
-export {GEODB_URL}
-export {GEODB_OPTIONS}
