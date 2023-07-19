90e4f77e052d104c6f7c91ac860edb3ebe6bc53a
shochmut <50213094+shochmut@users.noreply.github.com>
2023-07-19 08:06:09 -0600

    Fixed API Call, added input handler linked to useEffect function

--- src/CityInput.jsx
+++ src/CityInput.jsx
@@ -1,4 +1,6 @@
 import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import Stack from '@mui/material/Stack';
 import { useState, useEffect } from 'react';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -6,25 +8,31 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { GEODB_OPTIONS } from './geodbAPI.jsx';
 import { GEODB_URL } from './geodbAPI.jsx';
+import axios from 'axios';
 
 function CityInput() {
   const [cities, setCities] = useState([]);
+  const [input, setInput] = useState('');
 
   useEffect(() => {
+    console.log('useeffect has been called');
     fetch(GEODB_URL, GEODB_OPTIONS)
       .then((response) => response.json())
       .then((json) => setCities(json.data));
-  }, []);
+  }, [cities]);
   console.log(cities);
 
   return (
-    <TextField
-      id="city-input"
-      variant="outlined"
-      fullwidth
-      color="success"
-      focused
-    />
+    <Stack sx={{ width: 300, margin: 'auto' }}>
+      <Autocomplete
+        id="city-input"
+        freesolo
+        filterOptions={(x) => x} // disable filtering on client
+        options={cities}
+        onInputChange={(e, newInput) => setInput(newInput)}
+        renderInput={(params) => <TextField {...params} label="Combo box" />}
+      />
+    </Stack>
   );
 }
 
