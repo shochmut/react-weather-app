5d0a3edf940a4f58a06d903f7364707c7a6a4fc5
shochmut <50213094+shochmut@users.noreply.github.com>
2023-07-29 14:26:11 -0600

    1 file modified

--- src/Components/ForecastDisplay/ForecastDisplay.jsx
+++ src/Components/ForecastDisplay/ForecastDisplay.jsx
@@ -6,11 +6,21 @@ import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 function ForecastDisplay({ data }) {
   console.log(data);
 
+  const getDateTime = () => {
+    // get a new date (locale machine date time)
+    let date = new Date();
+    // get the date as a string
+    let n = date.toDateString();
+    // get the time as a string
+    let time = date.toLocaleTimeString();
+    return n + ' ' + time;
+  };
+
   return (
     <>
       <h1>Forecast</h1>
       <Stack direction="row" spacing={3} alignItems="center">
-        <h1>{data.list.dt}</h1>
+        <h1>{getDateTime(data.list[0].dt)}</h1>
         <h1>Test</h1>
       </Stack>
     </>);
